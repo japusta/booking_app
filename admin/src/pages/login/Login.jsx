85d6e751d6ad9ea79577acf24da319beddd4c9c6
japusta <99975684+japusta@users.noreply.github.com>
2023-09-10 21:43:49 +0300

    Add files via upload

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import React, { useContext } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../../context/authContext'
+import "./login.scss"
+
+const Login = () => {
+    const [credentials, setCredentials] = useState({
+        username:undefined,
+        password:undefined,
+    })
+    const {loading, error, dispatch} = useContext(AuthContext)
+
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setCredentials((prev) => ({...prev, [e.target.id]:e.target.value}))
+    }
+
+    const handleLogin =async e => {
+        e.preventDefault()
+        dispatch({type: "LOGIN_START"})
+        try{
+            const res =await axios.post("/auth/login", credentials)
+            dispatch({type: "LOGIN_SUCCESS", payload: res.data.details})
+            navigate("/")
+        }
+        catch(err){
+            dispatch({type: "LOGIN_FAILURE", payload: err.response.data})
+        }
+    }
+
+
+
+    return (
+        <div className='login'>
+            <div className="login-container">
+                <input type="text" placeholder='username' id='username' onChange={handleChange} className="login-container__input" />
+                <input type="password" placeholder='password' id='password' onChange={handleChange} className="login-container__input" />
+                <button disabled={loading} onClick={handleLogin} className="login-container__button">
+                    Login
+                </button>
+                {error && <span>{error.message}</span>}
+            </div>
+        </div>
+    )
+}
+
+export default Login
